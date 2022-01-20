6311a9ea39d6a846256cfecab18d4570cded4f97
Giovanna Ruggio <giovannaruggio@gmail.com>
2022-01-20 16:36:43 -0500

    JS files with updated functions to generateHTML

--- generateHTML.js
+++ generateHTML.js
@@ -1,59 +1,103 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for my team members and their information
-// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
-// WHEN I click on an email address in the HTML
-// THEN my default email program opens and populates the TO field of the email with the address
-// WHEN I click on the GitHub username
-// THEN that GitHub profile opens in a new tab
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated
-
 const inputArray = require('./index');
+const teamMembers = [];
 
-function renderTeamMembers(userInput) {
-    Object.keys(inputArray).forEach(teamMember => {
-        if (inputArray[teamMember].getRole() === 'Engineer') {
-            return renderEngineer(userInput)
-        } else if (inputArray[teamMember].getRole() === 'Intern') {
-            return renderIntern(userInput)
-        } else
-            return renderManager(userInput)
-    })
+
+function generateHTML(userInput) {
+    if(userInput){
+    userInput.map(employee => {
+        if (employee.getRole() === 'Manager') {
+            const managerCard = userInput.map(renderManager);
+            teamMembers.push(managerCard);
+        }
+        if (employee.getRole() === 'Engineer') {
+            const engineerCard = userInput.map(renderEngineer);
+            teamMembers.push(engineerCard);
+        }
+        if (employee.getRole() === 'Intern') {
+            const internCard = userInput.map(renderIntern);
+            teamMembers.push(internCard);
+        }
+    })}
+    return renderTeamMembers(teamMembers)
 }
 
 function renderManager(userInput) {
-    return ``
+  return `<div class="row section">
+    <div class="col s6 offset-s3">
+        <header class = "card-panel teal">
+            <i class="material-icons">account_box</i>
+            Manager: ${userInput.firstLast}
+        </header>
+      <div class="card-panel teal lighten-4">
+        <span class="grey darken-3-text">
+        <p>ID: ${userInput.employeeId}</p>
+        <p>Email: ${userInput.email}</p>
+        <p>Office Number: ${userInput.officeNum}</p>
+        </span>
+      </div>
+    </div>
+  </div>`
 }
 
 function renderEngineer(userInput) {
-    return ``
+    return `<div class="row section">
+    <div class="col s6 offset-s3">
+        <header class = "card-panel teal">
+            <i class="material-icons">account_box</i>
+            Engineer: ${userInput.firstLast}
+        </header>
+      <div class="card-panel teal lighten-4">
+        <span class="grey darken-3-text">
+        <p>ID: ${userInput.employeeId}</p>
+        <p>Email: ${userInput.email}</p>
+        <p>GitHub: ${userInput.githubUser}</p>
+        </span>
+      </div>
+    </div>
+  </div>`
 }
 
 function renderIntern(userInput) {
-    return ``
+    return `<div class="row section">
+    <div class="col s6 offset-s3">
+        <header class = "card-panel teal">
+            <i class="material-icons">account_box</i>
+            Intern: ${userInput.firstLast} 
+        </header>
+      <div class="card-panel teal lighten-4">
+        <span class="grey darken-3-text">
+        <p>ID: ${userInput.employeeId}</p>
+        <p>Email: ${userInput.email}</p>
+        <p>School: ${userInput.school}</p>
+        </span>
+      </div>
+    </div>
+</div>`
 }
 
-function generateHTML(userInput) {
+function renderTeamMembers(teamMembers) {
     return `<!DOCTYPE html>
     <html lang="en">
     <head>
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css">
+      <link rel="stylesheet" 
+      href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <title>Team Profile Generator</title>
     </head>
-    <header>Go Team</header>
+    <header>
+        <nav>
+            <div class="nav-wrapper deep-orange darken-1">
+              <a class="brand-logo center ">Our Team</a>
+            </div>
+          </nav>
+    </header>
     <body>
+    ${teamMembers}
     </body>
+    </html>
     `
 }
 
-module.exports = generateHTML;
+module.exports = generateHTML
--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for my team members and their information
-// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
-// WHEN I click on an email address in the HTML
-// THEN my default email program opens and populates the TO field of the email with the address
-// WHEN I click on the GitHub username
-// THEN that GitHub profile opens in a new tab
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated
-
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateHTML = require('./generateHTML');
@@ -23,6 +5,7 @@ const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
+
 const inputArray = [];
 
 const managerInput = [
@@ -112,47 +95,26 @@ const internInput = [
     }
 ]
 
-function inputManager() {
-    inquirer
-    .prompt(managerInput)
-    .then((userInput) => {
-        const addManager = new Manager(userInput);
-        inputArray.push(addManager)
-        switch (userInput.teamMember) {
-            case 'Manager':
-                init();
-                break;
-            case 'Engineer':
-                init();
-                break;
-            case 'Intern':
-                init();
-                break;
-            case 'I am finsihed building my team.':
-                writeToFile('./output/generateHTML.html', generateHTML(inputArray));
-                break;
-        }
-    });
-}
 
 function inputEngineer() {
     inquirer
     .prompt(engineerInput)
     .then((userInput) => {
-        const addEngineer = new Engineer(userInput);
+        const addEngineer = new Engineer(userInput.firstLast, userInput.employeeId, userInput.email, userInput.githubUser);
         inputArray.push(addEngineer)
         switch (userInput.teamMember) {
             case 'Manager':
                 init();
                 break;
             case 'Engineer':
-                init();
+                inputEngineer();
                 break;
             case 'Intern':
-                init();
+                inputIntern();
                 break;
             case 'I am finsihed building my team.':
-                writeToFile('./output/generateHTML.html', generateHTML(inputArray));
+                const renderHTML = generateHTML(inputArray)
+                writeToFile('./output/generateHTML.html', renderHTML);
                 break;
         }
     });
@@ -162,105 +124,60 @@ function inputIntern() {
     inquirer
     .prompt(internInput)
     .then((userInput) => {
-        const addIntern = new Intern(userInput);
+        const addIntern = new Intern(userInput.firstLast, userInput.employeeId, userInput.email, userInput.school);
         inputArray.push(addIntern)
         switch (userInput.teamMember) {
             case 'Manager':
                 init();
                 break;
             case 'Engineer':
-                init();
+                inputEngineer();
                 break;
             case 'Intern':
-                init();
+                inputIntern();
                 break;
             case 'I am finsihed building my team.':
-                writeToFile('./output/generateHTML.html', generateHTML(inputArray));
+                const renderHTML = generateHTML(inputArray)
+                writeToFile('./output/generateHTML.html', renderHTML);
+                break;
+        }
+    });
+}
+
+function init() {
+    inquirer
+    .prompt(managerInput)
+    .then((userInput) => {
+        const addManager = new Manager(userInput.firstLast, userInput.employeeId, userInput.email, userInput.officeNum);
+        inputArray.push(addManager)
+        switch (userInput.teamMember) {
+            case 'Manager':
+                init();
+                break;
+            case 'Engineer':
+                inputEngineer();
+                break;
+            case 'Intern':
+                inputIntern();
+                break;
+            case 'I am finsihed building my team.':
+                const renderHTML = generateHTML(inputArray)
+                writeToFile('./output/generateHTML.html', renderHTML);
                 break;
         }
     });
 }
 
 function writeToFile(fileName, data) {
-        fs.writeFile(fileName, data, (err) =>
-        err ? console.log(err) : console.log('Team successfully generated!')
-        );
-}
-
-function init() {
-    inquirer
-        .prompt(managerInput)
-        .then((userInput) => {console.log(userInput)
-            const fileNew = generateHTML(userInput);
-            const fileName = './output/generatedHTML.html';
-
-            writeToFile(fileName, fileNew);
-        })
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log('Team successfully generated!')
+    );
 }
 
 init();
 
-// function roleInput() {
-//     if (userInput.choices === 'Manager') {
-//         inquirer.prompt(managerInput);
-//     } else if (userInput.choices === 'Engineer') {
-//         inquirer.prompt(engineerInput);
-//     } else if (userInput.choices === 'Intern') {
-//         inquirer.prompt(internInput);
-//     } else if (userInput.choices === 'I am finsihed building my team.') {
-//         init();
-//     }
-// } 
 
-// function inputManager() {
-//     inquirer
-//     .prompt(managerInput)
-//     .then((userInput) => {
-//         const manager = new Manager(userInput);
-//         team.push(manager);
-//         console.log(team);
-//     })
-//     .catch(error => console.log(error));
-// }
 
-// function inputEngineer() {
-//     inquirer
-//     .prompt(engineerInput)
-//     .then((userInput) => {
-//         const engineer = new Engineer(userInput);
-//         team.push(engineer);
-//         console.log(team);
-//         inputRole();
-//     })
-//     .catch(error => console.log(error));
-// }
-
-// function inputIntern() {
-//     inquirer
-//     .prompt(internInput)
-//     .then((userInput) => {
-//         const intern = new Intern(userInput);
-//         team.push(intern);
-//         console.log(team);
-//         inputRole();
-//     })
-//     .catch(error => console.log(error));
-// }
-
-// function inputRole() {
-//     inquirer
-//     .prompt(newRole)
-//     .then((userInput) => {
-//         if (userInput.choices === 'Manager') 
-//             inputManager();
-//         if (userInput.choices === 'Engineer')
-//             inputEngineer();
-//         if (userInput.choices === 'Intern')
-//             inputIntern();
-//         if (userInput.choices === 'I am finsihed building my team.')
-//             writeToFile();
-//     })
-// }
 
 
 
